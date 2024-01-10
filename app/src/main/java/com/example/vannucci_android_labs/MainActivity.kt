845c02e3a19716556ffa1295a84efa65922ca669
Vannucci <homelete@gmail.com>
2024-01-10 10:21:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/vannucci_android_labs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vannucci_android_labs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
