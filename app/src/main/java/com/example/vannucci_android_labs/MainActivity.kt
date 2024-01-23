a8feeae5ec98a1a522b355ce26cc3b7b98b285b0
Vannucci <homelete@gmail.com>
2024-01-23 10:52:46 -0500

    Button , edit text and view text added and working. Variables changed from var to val.

--- app/src/main/java/com/example/vannucci_android_labs/MainActivity.kt
+++ app/src/main/java/com/example/vannucci_android_labs/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.vannucci_android_labs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mytext = findViewById<TextView>(R.id.textview)
+        val myedit = findViewById<EditText>(R.id.myedittext)
+        val mybutton = findViewById<Button>(R.id.button_id)
+
+        mybutton.setOnClickListener {
+            val editString = myedit.getText().toString()
+
+            mytext.setText("Your edit text has:" + editString)
+    }
     }
 }
